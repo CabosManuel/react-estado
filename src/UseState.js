2149d9e2e3889b09946032117fae9a01d4fd2b87
CabosManuel <cabos.apolaya@gmail.com>
2024-02-04 17:26:05 -0500

    useEffect for new loading state

--- src/UseState.js
+++ src/UseState.js
@@ -1,14 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function UseState({ name }) {
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    console.log('********* Start useEffect() *********');
+
+    if (loading) {
+      setTimeout(() => {
+        console.log('Validation data...');
+        setLoading(false);
+        console.log('Validation complete!');
+      }, 3000);
+    }
+
+    console.log('********** End useEffect() **********');
+  }, [loading]);
 
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>Escribe el código de seguridad.</p>
       <input placeholder="Código de seguridad"/>
-      <button onClick={() => setError(!error)}>Enviar</button>
+      <button onClick={() => setLoading(true)}>Enviar</button>
+
+      { (loading)
+        ? <p>Loading...</p>
+        : null
+      }
 
       { (error)
         ? <p style={{color: "red"}}>Error: useState "error" default value is true</p>
