1142131994c70651fc4970b571710e6d942f4262
CabosManuel <cabos.apolaya@gmail.com>
2024-02-04 20:08:47 -0500

    Lifecycle React.Component in loading state

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,13 +1,41 @@
 import { Component } from "react";
+import { Loading } from "./Loading";
 
 class ClassState extends Component {
-  // En las clases solo se puede crear 1 SOLO ESTADO
   constructor(props) {
     super(props);
 
+    // En las clases solo se puede crear 1 SOLO ESTADO (un objeto con varias propiedades)
     this.state = {
-      error: false
+      error: false,
+      loading: false,
     };
+  }
+
+  // Métodos del ciclo de vida:
+
+  // Se ejecuta inmediatamente después del constructor
+  UNSAFE_componentWillMount() {
+    console.log('UNSAFE_componentWillMount');
+  }
+
+  // Se ejecuta cuando el componente se agregue por primera vez (se monte)
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  // Se ejecuta inmediatamente después que el estado se actualize o una prop lo llame de nuevo
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+
+    // Solo cuando el estado de loading sea true, hacer un timeout y cambiar estado a false
+    if (this.state.loading) {
+      setTimeout(() => {
+        console.log('Validation data...');
+        this.setState({loading: false});
+        console.log('Validation complete!');
+      }, 2000);
+    }
   }
 
   render() {
@@ -18,12 +46,17 @@ class ClassState extends Component {
         <input placeholder="Código de seguridad"/>
         <button onClick={
           () =>
-            this.setState(prevState => ({error: !prevState.error}))
+            this.setState({loading: true})
           }
         >Enviar</button>
 
         { (this.state.error)
           ? <p style={{color: "red"}}>Error</p>
+          : null
+        }
+
+        { (this.state.loading)
+          ? <Loading/>
           : null
         }
       </div>
