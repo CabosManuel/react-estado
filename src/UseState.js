c8a8a471f8c3b917a4e07b9acd00d64e9a239647
CabosManuel <cabos.apolaya@gmail.com>
2024-03-04 23:13:23 -0500

    Create new state for input text in UseState.js

--- src/UseState.js
+++ src/UseState.js
@@ -1,18 +1,30 @@
 import { useEffect, useState } from "react";
 
+const SECURITY_CODE = 'usestate';
+
 function UseState({ name }) {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [value, setValue] = useState('');
+
+  console.log(value);
 
   useEffect(() => {
     console.log('********* Start useEffect() *********');
 
     if (loading) {
+      setError(false);
+
       setTimeout(() => {
         console.log('Validation data...');
+        if (value !== SECURITY_CODE) {
+          setError(true);
+        }
+
         setLoading(false);
+
         console.log('Validation complete!');
-      }, 3000);
+      }, 2000);
     }
 
     console.log('********** End useEffect() **********');
@@ -22,7 +34,13 @@ function UseState({ name }) {
     <div>
       <h2>Eliminar {name}</h2>
       <p>Escribe el código de seguridad.</p>
-      <input placeholder="Código de seguridad"/>
+      <input
+        placeholder="Código de seguridad"
+        value={value}
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+      />
       <button onClick={() => setLoading(true)}>Enviar</button>
 
       { (loading)
