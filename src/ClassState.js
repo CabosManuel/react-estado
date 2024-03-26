195488f1522fb4ebd8b430b3bd8d779885d6624a
CabosManuel <cabos.apolaya@gmail.com>
2024-03-25 22:07:06 -0500

    New state for input text in ClassState.js

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,6 +1,8 @@
 import { Component } from "react";
 import { Loading } from "./Loading";
 
+const SECURITY_CODE = 'classstate';
+
 class ClassState extends Component {
   constructor(props) {
     super(props);
@@ -9,6 +11,7 @@ class ClassState extends Component {
     this.state = {
       error: false,
       loading: false,
+      value: '',
     };
   }
 
@@ -32,30 +35,44 @@ class ClassState extends Component {
     if (this.state.loading) {
       setTimeout(() => {
         console.log('Validation data...');
-        this.setState({loading: false});
+
+        if (SECURITY_CODE === this.state.value) {
+          this.setState({ loading: false, error: false });
+        } else {
+          this.setState({ loading: false, error: true });
+        }
+
         console.log('Validation complete!');
-      }, 2000);
+      }, 1500);
     }
   }
 
   render() {
+    const { error, loading, value } = this.state;
+
     return (
       <div>
         <h2>Eliminar {this.props.name}</h2>
         <p>Escribe el código de seguridad.</p>
-        <input placeholder="Código de seguridad"/>
+        <input
+          placeholder="Código de seguridad"
+          value={value}
+          onChange={(event) => {
+            this.setState({ value: event.target.value });
+          }}
+        />
         <button onClick={
           () =>
             this.setState({loading: true})
           }
         >Enviar</button>
 
-        { (this.state.error)
+        { ( error && !loading )
           ? <p style={{color: "red"}}>Error</p>
           : null
         }
 
-        { (this.state.loading)
+        { (loading)
           ? <Loading/>
           : null
         }
