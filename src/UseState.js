7994a599ab6ebec66031c86c7e6ba550f6cc6c7d
CabosManuel <cabos.apolaya@gmail.com>
2024-05-04 18:13:48 -0500

    Imperative states, 2 new views: Confirmation, Deleted

--- src/UseState.js
+++ src/UseState.js
@@ -1,36 +1,47 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const SECURITY_CODE = 'usestate';
+const SECURITY_CODE = 'platzi';
 
 function UseState({ name }) {
   // useState Compuesto
   const [state, setState] = useState({
     error: false,
     loading: false,
-    value: ''
+    value: '',
+    deleted: false,
+    confirmed: false
   });
 
   // useState Independiente
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [value, setValue] = useState('');
+  // const [error, setError] = useState(false);
+  // const [loading, setLoading] = useState(false);
+  // const [value, setValue] = useState('');
 
-  console.log(state);
+  console.log('before', state);
 
   useEffect(() => {
     console.log('********* Start useEffect() *********');
 
+    // Si el estado de loading es true
     if (state.loading) {
-      setState({ ...state, ...state, error: false });
+      setState({ ...state, error: false }); // Estado de error: false
 
       setTimeout(() => {
         console.log('Validation data...');
         // if (value !== SECURITY_CODE) { // useSate Independiente
-        if (state.value !== SECURITY_CODE) { // useState Compuesto
-          setState({ ...state, error: true });
-        }
+        
+        // useState Compuesto ------------------------------------
+        if (state.value !== SECURITY_CODE) {
+          setState({ ...state, loading: false, error: true });
 
-        setState({ ...state, loading: false });
+          // Borrar el mensaje de error después de unos segundos
+          setTimeout(() => {
+            setState({ ...state, loading: false, error: false });
+          }, 5000);
+
+        } else {
+          setState({ ...state, loading: false, confirmed: true });
+        }
 
         console.log('Validation complete!');
       }, 2000);
@@ -39,30 +50,84 @@ function UseState({ name }) {
     console.log('********** End useEffect() **********');
   }, [state.loading]);
 
-  return (
-    <div>
-      <h2>Eliminar {name}</h2>
-      <p>Escribe el código de seguridad.</p>
-      <input
-        placeholder="Código de seguridad"
-        value={state.value}
-        onChange={(event) => {
-          setState({ ...state, value: event.target.value });
-        }}
-      />
-      <button onClick={() => setState({ ...state, loading: true })}>Enviar</button>
+  // Estos if están encapsulando diferentes vistas, diferentes return
+  if (!state.deleted && !state.confirmed) {
+    // Vista normal
+    return (
+      <div>
+        <h2>Eliminar {name}</h2>
+        <p>Escribe el código de seguridad.</p>
+        <input
+          placeholder="Código de seguridad"
+          value={state.value}
+          onChange={(event) => {
+            setState({ ...state, value: event.target.value });
+          }}
+        />
+        <button onClick={() => setState({ ...state, loading: true })}>Enviar</button>
+  
+        { (state.loading)
+          ? <p>Loading...</p>
+          : null
+        }
+  
+        { (state.error)
+          // ? <p style={{color: "red"}}>Error: useState "error" default value is true</p>
+          ? <p style={{color: "red"}}>Error: Código de seguridad incorrecto.</p>
+          : null
+        }
+      </div>
+    );
+  } else if (state.confirmed && !state.deleted) {
+    // Vista cuando sea confirmado y no eliminado
+    return (
+      <div>
+        <p>¿Estás seguro que quieres eliminar UseState?</p>
+        
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              deleted: true
+            });
+          }}
+        >
+          Confirmar
+        </button>
 
-      { (state.loading)
-        ? <p>Loading...</p>
-        : null
-      }
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              confirmed: false,
+              value: ''
+            });
+          }}
+        >
+          Cancelar
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>Deleted!</p>
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              confirmed: false,
+              deleted: false,
+              value: ''
+            });
+          }}
+        >
+          Deshacer eliminación
+        </button>
+      </div>
+    );
+  }
 
-      { (state.error)
-        ? <p style={{color: "red"}}>Error: useState "error" default value is true</p>
-        : null
-      }
-    </div>
-  );
 }
 
 export { UseState };
