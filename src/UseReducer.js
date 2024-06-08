357c7d7faf97bb0a37cb6ad24fc3fad5226b9f63
CabosManuel <cabos.apolaya@gmail.com>
2024-06-08 17:26:52 -0500

    Reducer: 3 way to create a Reducer

--- src/UseReducer.js
+++ src/UseReducer.js
@@ -9,5 +9,60 @@ const initialState = {
 // const reducer = (state, action) => {
 // }
 
-// const reducer = (state, action) => {
-// }
+// 1ra Forma de crear un Reducer: Usando condicionales
+const reducer = (state, action) => {
+  if(action.type === 'ERROR') {
+    return {
+      ...state, // El estado anterior
+      error: true,
+      loading: false,
+    };
+  } else if (action.type === 'CHECK') {
+    return {
+      ...state,
+      loading: true,
+    };
+  } else {
+    return {
+      ...state,
+    }
+  }
+}
+
+// 2ra Forma de crear un Reducer: Usando switch (más común)
+const reducerSwitch = (state, action) => {
+  switch (action.type) {
+    case 'ERROR':
+      return {
+        ...state, // El estado anterior
+        error: true,
+        loading: false,
+      };
+    case 'CHECK':
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return {
+        ...state,
+      }
+  }
+}
+
+// 3ra Forma de crear Reduce: Reduce Object
+const reducerObject = (state) => ({
+  'ERROR': {
+    ...state, // El estado anterior
+    error: true,
+    loading: false,
+  },
+  'CHECK': {
+    ...state,
+    loading: true,
+  }
+});
+
+const reducerAction = (state, action) => {
+  return reducerObject(state)[action.type] || state;
+}
