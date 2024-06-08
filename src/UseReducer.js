993e6500a42954b3b9144c7ea205dabba6711a80
CabosManuel <cabos.apolaya@gmail.com>
2024-06-08 16:53:01 -0500

    Reducer (new file)

---
+++ src/UseReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    error: false,
+    loading: false,
+    value: '',
+    deleted: false,
+    confirmed: false,
+}
+
+// const reducer = (state, action) => {
+// }
+
+// const reducer = (state, action) => {
+// }
